13660ec46f2e9fb7defe0f6de13ba057b4a8a085
Warren MacEvoy <wmacevoy@gmail.com>
2020-09-03 12:30:31 -0600

    tested.

---
+++ Objects/app/src/test/java/edu/coloradomesa/cs/objects/WellTest.java
@@ -0,0 +1,32 @@
+package edu.coloradomesa.cs.objects;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class WellTest {
+
+    @Test
+    public void getDepth() {
+    }
+
+    @Test
+    public void isDry() {
+    }
+
+    @Test
+    public void getWaterLevel() {
+    }
+
+    @Test
+    public void setWaterLevel() {
+    }
+
+    @Test
+    public void isPoisoned() {
+    }
+
+    @Test
+    public void setPoisoned() {
+    }
+}
