80871cc047e16b06c57d99e85f112976eaffb29c
Warren MacEvoy <wmacevoy@gmail.com>
2020-09-03 22:49:21 -0600

    filled out tests (100% coverage)

--- Objects/app/src/test/java/edu/coloradomesa/cs/objects/WellTest.java
+++ Objects/app/src/test/java/edu/coloradomesa/cs/objects/WellTest.java
@@ -6,27 +6,68 @@ import static org.junit.Assert.*;
 
 public class WellTest {
 
-    @Test
-    public void getDepth() {
+    @Test(expected = IllegalArgumentException.class)
+    public void invalidDepth() throws Exception {
+        double wellDepth = -35.2;
+        Well instance = new Well(wellDepth);
+        assertEquals(wellDepth, instance.getDepth(), 0.0);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void invalidWaterLevel() throws Exception {
+        double wellDepth = 35.2;
+        Well instance = new Well(wellDepth);
+        double invalidWaterLevel = -1.0;
+        instance.setWaterLevel(invalidWaterLevel);
     }
 
     @Test
-    public void isDry() {
+    public void getDepth() {
+        double wellDepth = 35.2;
+        Well instance = new Well(wellDepth);
+        assertEquals(wellDepth, instance.getDepth(), 0.0);
+    }
+
+    @Test
+    public void isDryTrue() {
+        double wellDepth = 35.2;
+        Well instance = new Well(wellDepth);
+        assertEquals(true, instance.isDry());
+    }
+
+    @Test
+    public void isDryFalse() {
+        double wellDepth = 35.2;
+        Well instance = new Well(wellDepth);
+        double waterLevel = 10.0;
+        instance.setWaterLevel(waterLevel);
+        assertEquals(false, instance.isDry());
     }
 
     @Test
     public void getWaterLevel() {
+        double wellDepth = 35.2;
+        Well instance = new Well(wellDepth);
+        assertEquals(0.0, instance.getWaterLevel(), 0.0);
     }
 
     @Test
     public void setWaterLevel() {
+        double wellDepth = 35.2;
+        Well instance = new Well(wellDepth);
+        assertEquals(0.0, instance.getWaterLevel(), 0.0);
+        double newWaterLevel = 13.5;
+        instance.setWaterLevel(newWaterLevel);
+        assertEquals(newWaterLevel, instance.getWaterLevel(), 0.0);
     }
 
     @Test
-    public void isPoisoned() {
-    }
-
-    @Test
-    public void setPoisoned() {
+    public void poisoned() {
+        double depth = 35.2;
+        Well instance = new Well(depth);
+        double waterLevel = 13.5;
+        assertEquals(false,instance.isPoisoned());
+        instance.setPoisoned(true);
+        assertEquals(true,instance.isPoisoned());
     }
 }
