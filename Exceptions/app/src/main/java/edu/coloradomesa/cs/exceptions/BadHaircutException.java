9d78a594fef6b5e2706bc9e020e6be308276b8a7
Warren MacEvoy <wmacevoy@gmail.com>
2020-09-10 14:51:03 -0600

    custom exception example

--- Exceptions/app/src/main/java/edu/coloradomesa/cs/exceptions/BadHaircutException.java
+++ Exceptions/app/src/main/java/edu/coloradomesa/cs/exceptions/BadHaircutException.java
@@ -1,4 +1,14 @@
 package edu.coloradomesa.cs.exceptions;
 
-public class BadHaircutException {
+public class BadHaircutException extends Exception {
+    public BadHaircutException() { super(); }
+    public BadHaircutException(String message) {
+        super(message);
+    }
+    public BadHaircutException(String message, Throwable cause) {
+        super(message,cause);
+    }
+    public BadHaircutException(Throwable cause) {
+        super(cause);
+    }
 }
--- Exceptions/app/src/main/java/edu/coloradomesa/cs/exceptions/Broken.java
+++ Exceptions/app/src/main/java/edu/coloradomesa/cs/exceptions/Broken.java
@@ -1,4 +1,33 @@
 package edu.coloradomesa.cs.exceptions;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
 public class Broken {
+    public void indexOutOfBounds() {
+        int [] a = new int[10];
+
+        a[10]=11;
+
+    }
+
+    public void ioExceptionThrows() throws FileNotFoundException {
+        FileInputStream in = new FileInputStream("missing file.dat");
+    }
+
+    public void ioExceptionCatch() {
+        try {
+            FileInputStream in = new FileInputStream("missing file.dat");
+        } catch (FileNotFoundException e) {
+            InputStream is = System.in;
+            // ...
+        }
+    }
+
+    public void cutHair() throws BadHaircutException {
+        ///
+        throw new BadHaircutException("wow that's a bad haircut!");
+    }
 }
