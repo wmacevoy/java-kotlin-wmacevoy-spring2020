a2c837cfcb201be1e37a0774bfb33b542ef49fb8
Warren MacEvoy <wmacevoy@gmail.com>
2020-12-01 13:02:14 -0700

    add better randomness

--- Threads/app/src/test/java/edu/coloradomesa/cs/threads/MainTest.java
+++ Threads/app/src/test/java/edu/coloradomesa/cs/threads/MainTest.java
@@ -2,18 +2,35 @@ package edu.coloradomesa.cs.threads;
 
 import org.junit.Test;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 import static org.junit.Assert.*;
 
 public class MainTest {
-    String randomString() {
-        int len = (int)(Math.random()*10+1);
+    int random(int min, int max) {
+        int range = max - min + 1;
+        int value = min + (range > 1 ? ThreadLocalRandom.current().nextInt(range) : 0);
+        return value;
+    }
+
+    String random(int minLen, int maxLen, int[] codePoints) {
+        int len = random(minLen,maxLen);
         StringBuilder sb = new StringBuilder(len);
         for (int i=0; i<len; ++i) {
-            char c = (char)('a' + (int)(Math.random()*26));
-            sb.append(c);
+            int cp = codePoints[random(0,codePoints.length-1)];
+            sb.appendCodePoint(cp);
         }
-        return sb.toString();
+        String value = sb.toString();
+        return value;
     }
+
+    static final String LETTER_STRING = "123abd😀🤪🧐";
+    static final int[] LETTERS = LETTER_STRING.codePoints().toArray();
+
+    String randomString() {
+        return random(1,10,LETTERS);
+    }
+
     Message random() {
         return new Message(randomString(),randomString());
     }
